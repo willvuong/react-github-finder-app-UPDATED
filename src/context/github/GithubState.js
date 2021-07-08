9c16e389e632a0534bd158a84e707cc49e7503a2
William <wvuong3@myseneca.ca>
2021-07-08 15:35:21 -0400

    added github context files

---
+++ src/context/github/GithubState.js
@@ -0,0 +1,23 @@
+import React, { useReducer } from 'react'
+import axios from 'axios'
+import githubContext from './githubContext'
+import githubReducer from './githubReducer'
+
+import {
+    SEARCH_USERS,
+    SET_LOADING,
+    CLEAR_USERS,
+    GET_USER,
+    GET_REPOS
+} from '../types'
+
+const GithubState = props => {
+    const initialState = {
+        users: [],
+        user: {},
+        repos: [],
+        loading: false
+    }
+
+    const [state, dispatch] = useReducer(githubReducer, initialState);
+}
