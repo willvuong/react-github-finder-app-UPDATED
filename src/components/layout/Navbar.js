43df12f70b1bc37bfc69847eed242cc27bf75b80
William <wvuong3@myseneca.ca>
2021-08-03 14:42:00 -0400

    navbar edit

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const Navbar = ({ icon, title }) => {
+    const [activeTab, setActiveTab] = useState('')
+
+    //using this useEffect to make sure that the active tab is still highlighted when refreshing the page
+    const currentLocation = window.location.href;
+    useEffect(() => {
+        if (currentLocation.endsWith('/')) {
+            setActiveTab('Home')
+        } else if (currentLocation.endsWith('/about')) {
+            setActiveTab('About')
+        }
+    }, [activeTab])
+
     return (
-        <nav className="navbar bg-primary">
-            <i className={icon}> {title}</i>
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/about'>About</Link>
-                </li>
-            </ul>
-        </nav>
+        <div>
+            <nav className="navbar bg-primary">
+                <i className={icon}> {title}</i>
+                <ul>
+                    <li className={activeTab === 'Home' ? "navActive" : ""}><Link to='/' onClick={() => setActiveTab('Home')}>Home</Link></li>
+                    <li className={activeTab === 'About' ? "navActive" : ""}><Link to='/about' onClick={() => setActiveTab('About')}>About</Link></li>
+                </ul>
+            </nav>
+        </div>
+
     )
 }
 
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -11,6 +11,17 @@ import {
     GET_REPOS
 } from '../types'
 
+let githubClientId;
+let githubClientSecret;
+
+if(process.env.NODE_ENV !== 'production') {
+    githubClientId = process.env.REACT_APP_GITHUB_FINDER_APP_CLIENT_ID;
+    githubClientSecret = process.env.REACT_APP_GITHUB_FINDER_APP_CLIENT_SECRET;
+} else {
+    githubClientId = process.env.GITHUB_FINDER_APP_CLIENT_ID;
+    githubClientSecret = process.env.GITHUB_FINDER_APP_CLIENT_SECRET;
+}
+
 const GithubState = props => {
     const initialState = {
         users: [],
@@ -24,7 +35,7 @@ const GithubState = props => {
     // Search Users
     const searchUsers = async (text) => {
         setLoading();
-        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_FINDER_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_FINDER_APP_CLIENT_SECRET}`);
+        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`);
         dispatch ({type: SEARCH_USERS, payload: res.data.items})
 
       }
@@ -33,14 +44,14 @@ const GithubState = props => {
     // Get User
     const getUser = async (username) => {
         setLoading();
-        const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_FINDER_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_FINDER_APP_CLIENT_SECRET}`);
+        const res = await axios.get(`https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`);
         dispatch({ type: GET_USER, payload: res.data});
       }
 
     // Get Repos 
     const getUserRepos = async (username) => {
         setLoading();
-        const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_FINDER_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_FINDER_APP_CLIENT_SECRET}`);
+        const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`);
         dispatch({ type: GET_REPOS, payload: res.data})
     }
 
